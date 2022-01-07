0dc2d6935fde0123791bc6bfd865975a957bc793
Jennifer Glauche <mail@jglauche.de>
2022-01-07 17:33:29 +0100

    add local storage for nickname

--- src/js/stage/title.js
+++ src/js/stage/title.js
@@ -1,28 +1,46 @@
 import {Stage, Text, state, event, game, input, plugins} from 'melonjs/dist/melonjs.module.js';
 import PlayScreen from 'js/stage/play.js';
 
+function gameScreen() {
+  let save = document.getElementById('savesettings').checked;
+  if (save == true){
+    localStorage.setItem('nick', document.getElementById('name').value);
+  } else {
+    localStorage.clear();
+  }
+
+  state.set(state.PLAY, new PlayScreen("lobby"));
+  state.change(state.PLAY);
+}
+
+
 class TitleScreen extends Stage {
   onResetEvent() {
     plugins.keys.title();
 
-    this.setMenu('inherit');
-    document.getElementById('start').addEventListener('click', btn => {
-      state.set(state.PLAY, new PlayScreen("lobby"));
-      state.change(state.PLAY);
-    });
+    let nick = localStorage.getItem('nick');
+    if (nick != null){
+      document.getElementById('name').value = nick;
+      document.getElementById('savesettings').checked = true;
+    }
 
+    this.setMenu('inherit');
     this.handler = event.on(event.KEYDOWN, function (action, keyCode, edge) {
       if (action === "enter") {
-        state.set(state.PLAY, new PlayScreen("lobby"));
-        state.change(state.PLAY);
+        gameScreen();
       }
     });
+    document.getElementById('start').addEventListener('click', btn => {
+      gameScreen();
+    });
+
   }
+
 
   setMenu(display){
     let menu = document.getElementById('menu-overlay').style;
     menu.setProperty("display", display);
- }
+  }
 
   onDestroyEvent() {
     plugins.keys.game();
